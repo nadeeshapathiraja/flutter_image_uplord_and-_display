c0a9a8965874b7c06c0e18dc866b5edf2eabcc06
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-01-25 12:10:04 +0530

    Firebase indicated project

---
+++ lib/view/image_uplord.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImageUplord extends StatefulWidget {
+  const ImageUplord({Key? key}) : super(key: key);
+
+  @override
+  _ImageUplordState createState() => _ImageUplordState();
+}
+
+class _ImageUplordState extends State<ImageUplord> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
